b97768efc5aa947d9dcad3835ad07121925293f1
Kyle Kalantzis <kmk1052@wildcats.unh.edu>
2021-12-06 10:34:09 -0500

    added delete css to mil

--- military/delete.php
+++ military/delete.php
@@ -9,17 +9,18 @@ $conn = new mysqli($servername, $username, $password, $database);
 ?>
 <!DOCTYPE HTML>
 <HEAD>
+<link rel="stylesheet" href="http://localhost/COMP780_Project/military/delete.css?v=<?php echo time(); ?>">
 <meta charset = "UTF-8">
 <meta http-equiv ="X-UA-Compatible" content="IE=edge">
 <meta name = "viewport" content = "width=device-width, initial-scale = 1.0">
 </head>
 <body>
 <a href="http://localhost/COMP780_Project/pns.php">Go Back</a>
-<h3> Please Enter Badge Number to delete</h3>
-<h3> *WARNING ALL INFORMATION WILL BE DELETED ASSOCIATED WITH BADGE NUMBER*</h3>
+<h3> Please Enter ID Number to delete</h3>
+<h3> *WARNING ALL INFORMATION WILL BE DELETED ASSOCIATED WITH ID NUMBER*</h3>
 
 <form action="" method="POST">
-    Military Badge: <input type="number" name="mil_id"><br>
+    Military ID: <input type="number" name="mil_id"><br>
     <input type="submit">
 </form>
 <?php
