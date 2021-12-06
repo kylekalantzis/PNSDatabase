35d1003b383bde0804005adb2d5ea2b112f636b4
Kyle Kalantzis <kmk1052@wildcats.unh.edu>
2021-12-06 10:30:03 -0500

    added css to delete and ref stylesheet

--- contractors/delete.php
+++ contractors/delete.php
@@ -9,6 +9,7 @@ $conn = new mysqli($servername, $username, $password, $database);
 ?>
 <!DOCTYPE HTML>
 <HEAD>
+<link rel="stylesheet" href="http://localhost/COMP780_Project/contractors/delete.css?v=<?php echo time(); ?>">
 <meta charset = "UTF-8">
 <meta http-equiv ="X-UA-Compatible" content="IE=edge">
 <meta name = "viewport" content = "width=device-width, initial-scale = 1.0">
